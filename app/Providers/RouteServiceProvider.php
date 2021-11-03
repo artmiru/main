bf43e45ca89ba7c9edc5c0f014c94cca01895e11
Евсей <artmiru@gmail.com>
2021-11-03 20:12:35 +0300

    вроде все работает сделал пару миграций

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const ACCOUNT = '/account';
+    public const HOME = '/home';
 
     /**
      * The controller namespace for the application.
