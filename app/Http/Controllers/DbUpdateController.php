1d32a94627642c7c3c99e8a02ca265bec1eea42f
Евсей <artmiru@gmail.com>
2021-11-26 02:13:36 +0300

    настроил страницу с update db 2

--- app/Http/Controllers/DbUpdateController.php
+++ app/Http/Controllers/DbUpdateController.php
@@ -16,8 +16,112 @@ class DbUpdateController extends Controller
 //        $this->phoneFormat('a1_clients');
 //        $this->phoneFormat('new_students');
 //        $this->mkUp();
-//        $this->userUp();
+        $this->userUp();
 //        $this->abonementsUp();
+//        $this->visitsUp();
+    }
+
+    public function visitsUp()
+    {
+        $res = DB::unprepared("
+INSERT INTO visits( old_visit_id, user_id, aid, sid, used_hour, abonement_id, mk_id, visit_status_id, date_time, comments, sms, created_at) SELECT * FROM( SELECT crs_visit_records.id, users.id AS user_id, users.aid, users.sid, crs_visit_records.hours, crs_visit_records.id_abonement, NULL AS mk_id, crs_visit_records.id_status, crs_visit_records.date_time, crs_visit_records.rec_comments, crs_visit_records.sms, crs_visit_records.reg_date FROM crs_visit_records LEFT JOIN users ON users.aid = crs_visit_records.id_client UNION SELECT NULL AS old_visit_id, users.id AS user_id, users.aid, users.sid, 3 AS used_hour, NULL AS ab_id, mks.id AS mk_id, new_students.state, new_students.date_time, new_students.comments, new_students.sms, new_students.sign_date FROM new_students LEFT JOIN users ON users.name = new_students.name AND users.family = new_students.family AND users.phone = new_students.phone LEFT JOIN mks ON mks.date_time = new_students.date_time ) AS X ORDER BY X.reg_date
+");
+
+
+//статус надо отдельным запросом
+        if ($res) {
+            $vs = DB::table('visits')->select('visits.visit_status_id', 'visits.comments', 'visits.id')->get();
+            foreach ($vs as $v) {
+                if ($v->visit_status_id == (1 || 2)) {
+                    $status = 1;
+                } elseif ($v->visit_status_id == 3) {
+                    $status = 2;
+                } elseif ($v->visit_status_id == 4) {
+                    $status = 3;
+                } elseif ($v->visit_status_id == 5) {
+                    $status = 4;
+                }
+                DB::table('visits')
+                    ->where('id', '=', $v->id)
+                    ->update([
+                        'comments' => $v->comments . ' | ' . $v->visit_status_id,
+                        'visit_status_id' => $status,
+                        'updated_at' => now()
+                    ]);
+            }
+            foreach ($vs as $v) {
+                switch ($v->visit_status_id) {
+                    case 'Оплата на МК':
+                    case 'Оплатит на МК':
+                    case 'Оплатит в студии':
+                    case 'Напомнили':
+                    case 'Подарочный':
+                    case 'Оплачено':
+                    case 'Подарочный777':
+                    case 'Записан':
+                    case 'Заявка на запись':
+                        $status = 1;
+                        break;
+                    case 'Пришел на МК':
+                    case 'Частичный возврат':
+                    case 'Не купил':
+                    case 'Пришел':
+                        $status = 2;
+                        break;
+                    case 'Отказался':
+                    case 'Возврат':
+                    case 'Ошибка':
+                        $status = 4;
+                        break;
+                    case 'в ожидании платежа':
+                    case 'Перезвонить':
+                    case 'В ожидании':
+                        $status = 6;
+                        break;
+                    case 'Не пришел':
+                        $status = 3;
+                        break;
+                    case 'Выбирает МК':
+                    case 'Выбирает':
+                        $status = 5;
+                        break;
+                }
+
+                DB::table('visits')
+                    ->where('id', '=', $v->id)
+                    ->update([
+                        'comments' => $v->comments . ' | ' . $v->visit_status_id,
+                        'visit_status_id' => $status,
+                        'updated_at' => now()
+                    ]);
+            }
+        }
+        echo 'visitsUp ok';
+    }
+
+    public function phoneFormat($table = '')
+    {
+        $phones = DB::table($table)
+//            ->where('reg_date', '>', '2021-01-01 00:00:00')
+            ->get();
+
+        foreach ($phones as $phone) {
+            $p = preg_replace('/\D/', '', $phone->phone);
+
+            if (trim($phone->phone) !== trim($p)) {
+//                echo $phone->id.' = '.$phone->phone . ' - ' . $p . '<br>';
+                $p = trim($p);
+                if (empty($p)) {
+                    $p = NULL;
+                }
+                DB::table($table)
+                    ->where('id', $phone->id)
+                    ->update(['phone' => $p]);
+            } else {
+//                echo $phone->phone . '  ' . $p . '<br>';
+            }
+        }
+        echo 'ok';
     }
 
     public function abonementsUp()
@@ -38,68 +142,9 @@ INSERT INTO mks( `date_time`, `pic_id`, `teacher_id`, `price_id`, `created_at` )
     {
         //обьединение a1_clients  and new students
         //юсер наме и пароль генерировать
-        DB::unprepared("
-INSERT INTO users(
-    `aid`,
-    `sid`,
-    `name`,
-    `family`,
-    `patronymic`,
-    `phone`,
-    `email`,
-    `comments`,
-    `cookie`,
-    `blist`,
-    `created_at`
-)
-SELECT
-    *
-FROM
-    (
-    SELECT
-        a1.id AS aid,
-        a2.id AS sid,
-        a2.name,
-        a2.family,
-        a2.surname,
-        a2.phone,
-        a2.email,
-        a2.comments,
-        a2.user_id,
-        a2.blist,
-        a2.sign_date
-    FROM
-        new_students AS a2
-    LEFT JOIN a1_clients AS a1
-    ON
-        a2.name = a1.name AND a1.family = a2.family AND a1.phone = a2.phone
-    UNION
-SELECT
-    a1.id AS aid,
-    a2.id AS sid,
-    a1.name,
-    a1.family,
-    a1.patronymic,
-    a1.phone,
-    a1.email,
-    a1.comments,
-    a1.cookie_id,
-    a1.blist,
-    a1.reg_date
-FROM
-    new_students AS a2
-RIGHT JOIN a1_clients AS a1
-ON
-    a2.name = a1.name AND a1.family = a2.family AND a1.phone = a2.phone
-) AS u
-WHERE
-    `family` <> '' OR `phone` <> ''
-GROUP BY
-    `name`,
-    `family`,
-    `phone`;
-UPDATE `users` SET `username` = LPAD( FLOOR(RAND() * 999999.99), 5, '0'), `password` = LPAD( FLOOR(RAND() * 999999.99), 5, '0');
-");
+        DB::unprepared("INSERT INTO users( `aid`, `sid`, `name`, `family`, `patronymic`, `phone`, `email`, `comments`, `cookie`, `blist`, `created_at`) SELECT * FROM( SELECT a1.id AS aid, a2.id AS sid, a2.name, a2.family, a2.surname, a2.phone, a2.email, a2.comments, a2.user_id, a2.blist, a2.sign_date FROM new_students AS a2 LEFT JOIN a1_clients AS a1 ON a2.name = a1.name AND a1.family = a2.family AND a1.phone = a2.phone UNION SELECT a1.id AS aid, a2.id AS sid, a1.name, a1.family, a1.patronymic, a1.phone, a1.email, a1.comments, a1.cookie_id, a1.blist, a1.reg_date FROM new_students AS a2 RIGHT JOIN a1_clients AS a1 ON a2.name = a1.name AND a1.family = a2.family AND a1.phone = a2.phone ) AS u WHERE `family` <> '' OR `phone` <> '' GROUP BY `name`, `family`, `phone`; UPDATE `users` SET `username` = LPAD( FLOOR(RAND() * 999999.99), 5, '0'), `password` = LPAD( FLOOR(RAND() * 999999.99), 5, '0');");
+        // обновить скидку у юзера c crs_ab_records
+        DB::unprepared("UPDATE users SET users.discount_id =( SELECT MAX(crs_ab_records.id_discount) FROM crs_ab_records WHERE crs_ab_records.id_client = users.aid GROUP BY crs_ab_records.id_client);");
         echo 'userUp ok';
     }
 
@@ -136,107 +181,6 @@ UPDATE `users` SET `username` = LPAD( FLOOR(RAND() * 999999.99), 5, '0'), `passw
             $user_id = $hour->user_id;
         }
     }
-
-    public function mkVisitsUp()
-    {
-//        INSERT INTO visits( `user_id`, `used_hour`, `abonement_id`, `mk_id`, `visit_status_id`, `date_time`, `comments`, `created_at` ) SELECT users.id, 3.0, NULL, mks.id, NULL, new_students.date_time, new_students.comments, new_students.sign_date FROM new_students LEFT JOIN visits ON visits.created_at = new_students.sign_date JOIN users ON users.phone = new_students.phone JOIN mks ON mks.date_time = new_students.date_time WHERE new_students.date_time > '2021-11-01 00:00:00' AND visits.created_at IS NULL;
-//статус надо отдельным запросом
-        $vs = DB::table('visits1')
-            ->select('new_students.state', 'visits1.comments', 'visits1.id')
-            ->where('visit_status_id', '=', NULL)
-            ->join('users1', 'users1.id', '=', 'visits1.user_id')
-            ->join("new_students", function ($join) {
-                $join->on("new_students.name", "=", "users1.name")
-                    ->on("new_students.family", "=", "users1.family")
-                    ->on("new_students.phone", "=", "users1.phone")
-                    ->on("new_students.date_time", "=", "visits1.date_time");
-            })
-            ->get();
-
-        foreach ($vs as $v) {
-            switch ($v->state) {
-                case 'Оплата на МК':
-                case 'Оплатит на МК':
-                case 'Оплатит в студии':
-                case 'Напомнили':
-                case 'Подарочный':
-                case 'Оплачено':
-                case 'Подарочный777':
-                case 'Записан':
-                case 'Заявка на запись':
-                    $status = 1;
-                    break;
-                case 'Пришел на МК':
-                case 'Частичный возврат':
-                case 'Не купил':
-                case 'Пришел':
-                    $status = 2;
-                    break;
-                case 'Отказался':
-                case 'Возврат':
-                case 'Ошибка':
-                    $status = 4;
-                    break;
-                case 'в ожидании платежа':
-                case 'Перезвонить':
-                case 'В ожидании':
-                    $status = 6;
-                    break;
-                case 'Не пришел':
-                    $status = 3;
-                    break;
-                case 'Выбирает МК':
-                case 'Выбирает':
-                    $status = 5;
-                    break;
-            }
-
-            DB::table('visits1')
-                ->where('id', '=', $v->id)
-                ->update([
-                    'comments' => $v->comments . ' | ' . $v->state,
-                    'visit_status_id' => $status
-                ]);
-        }
-        echo 'ok';
-    }
-
-
-    public function crsVisitsUp()
-    {
-//        INSERT INTO `visits`( `old_visit_id`, `user_id`, `used_hour`, `abonement_id`, `visit_status_id`, `date_time`, `comments`, `sms`, `created_at` ) SELECT `id`, `id_client`, `hours` * 2, `id_abonement`, `id_status`, `date_time`, `rec_comments`, `sms`, `reg_date` FROM `crs_visit_records`;
-    }
-
-    public function phoneFormat($table = '')
-    {
-        $phones = DB::table($table)
-//            ->where('reg_date', '>', '2021-01-01 00:00:00')
-            ->get();
-
-        foreach ($phones as $phone) {
-            $p = preg_replace('/\D/', '', $phone->phone);
-
-            if (trim($phone->phone) !== trim($p)) {
-//                echo $phone->id.' = '.$phone->phone . ' - ' . $p . '<br>';
-                $p = trim($p);
-                if (empty($p)) {
-                    $p = NULL;
-                }
-                DB::table($table)
-                    ->where('id', $phone->id)
-                    ->update(['phone' => $p]);
-            } else {
-//                echo $phone->phone . '  ' . $p . '<br>';
-            }
-        }
-        echo 'ok';
-    }
-
-//    a1_clients -> users
-//    new_students -> users
-//crs_ab_records -> abonements ->payments
-//    crs_visit_records ->visits
-//    mk_records ->mks
 
 
 }
